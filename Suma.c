369f9dd16a44a7cf56784777e1f1b88722e9d87a
Stheeven Quishpe <stheeven.quishpe@udla.edu.ec>
2023-11-28 14:46:51 -0500

    Se inicializaron las variables que se van a usar

---
+++ Suma.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+int main(){
+srand (time(NULL));
+int n,m;
+printf("Coloque los datos de n\n");
+scanf("%d", &n);
+printf("Coloque los datos de m\n");
+scanf("%d", &m);
+
+int matrix[n][m];
+int matrix1[n][m];
+int matrix2[n][m];
+
